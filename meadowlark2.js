0d67cbef0702bef92f3821fde3d966828e939048
shaji@shaji.co.uk <5hajiGit>
2014-11-13 20:09:33 +0000

    Added routes file

--- meadowlark2.js
+++ meadowlark2.js
@@ -4,6 +4,9 @@ var fs = require('fs');
 
 var app = express();
 
+var routes = require('./routes/routes')(app);
+
+
 var hbsConfig = hbs.express3(
     {
         defaultLayout:__dirname + '/views/layouts/tes-header.hbs',
@@ -21,48 +24,7 @@ app.use(express.static(__dirname + '/public'));
 
 registerPartials(hbs.partialsDir);
 
-var fortunes = [
-    "Conquer your fears or they will conquer you.", "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.", "Whenever possible, keep it simple.",
-];
 
-var locations = [
-        {
-            href: '/jobs/',
-            title: 'JOBS'
-        },
-        {
-            href: '/teaching-resources/',
-            title: 'RESOURCES'
-        },
-        {
-            href: 'http://community.tes.co.uk',
-            title: 'COMMUNITY'
-        },
-       {
-           href: 'http://news.tes.co.uk',
-           title: 'SCHOOL NEWS'
-       },
-       {
-           href: 'http://news.tes.co.uk/further-education/',
-           title: 'FE NEWS'
-       }
-    ];
-
-app.get('/', function (req,res){
-    //res.type('text/plain');
-    //res.send('Meadowlark Travel');
-    res.render('home',{locationsData: locations});
-});
-
-app.get('/about', function(req,res){
-    //res.type('text/plain');
-    //res.send('About Meadowlark Travel');
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {fortune: randomFortune});
-
-});
 
 //var formidable = require('formidable');
 //
@@ -87,24 +49,7 @@ app.get('/about', function(req,res){
 //})
 
 
-//custom 404 page
-app.use(function(req,res){
-    //res.type('text/plain');
-    res.status(404);
-    //res.send('404 - Not found');
-    res.render('404');
-});
-
-
-//custom 500 page
-app.use(function(err, req, res, next){
-    console.error(err.stack);
-    //res.type('text/plain');
-    res.status(500);
-    //res.send('500 - Server Error');
-    res.render('500');
-});
-
+//register all partials
 function registerPartials(partialsFolder) {
 
     // get all the partial filenames in the partials folder
@@ -121,7 +66,6 @@ function registerPartials(partialsFolder) {
         hbs.registerPartial(name, template);
     });
 }
-
 
 app.listen(app.get('port'), function(){
     console.log('Express started on localhost:' + app.get('port'));
--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,56 @@
-//
-//module.exports = function(app) {
-//    app.get('/', function (req, res) {
-//        //res.type('text/plain');
-//        //res.send('Meadowlark Travel');
-//        res.render('home');
-//    });
 
-//    app.get('/about', function (req, res) {
-//        //res.type('text/plain');
-//        //res.send('About Meadowlark Travel');
-//        var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-//        res.render('about', {fortune: randomFortune});
-//
-//    });
-}
+module.exports = function(app) {
+
+    app.get('/', function (req, res) {
+        var locations = [
+            {
+                href: '/jobs/',
+                title: 'JOBS'
+            },
+            {
+                href: '/teaching-resources/',
+                title: 'RESOURCES'
+            },
+            {
+                href: 'http://community.tes.co.uk',
+                title: 'COMMUNITY'
+            },
+            {
+                href: 'http://news.tes.co.uk',
+                title: 'SCHOOL NEWS'
+            },
+            {
+                href: 'http://news.tes.co.uk/further-education/',
+                title: 'FE NEWS'
+            }
+        ];
+
+        res.render('home',{locationsData: locations});
+    });
+
+    app.get('/about', function (req, res) {
+
+        var fortunes = [
+            "Conquer your fears or they will conquer you.", "Rivers need springs.",
+            "Do not fear what you don't know.",
+            "You will have a pleasant surprise.", "Whenever possible, keep it simple.",
+        ];
+        var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+        res.render('about', {fortune: randomFortune});
+
+    });
+
+    //custom 404 page
+    app.use(function(req,res){
+        res.status(404);
+        res.render('404');
+    });
+
+//custom 500 page
+    app.use(function(err, req, res, next){
+        console.error(err.stack);
+        res.status(500);
+        res.render('500');
+    });
+
+};
