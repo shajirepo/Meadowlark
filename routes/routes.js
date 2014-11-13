84a263374002cf7fed8551a3a491254b98f58390
shaji@shaji.co.uk <5hajiGit>
2014-11-13 20:19:47 +0000

    Tidied up code

--- routes/routes.js
+++ routes/routes.js
@@ -1,40 +1,32 @@
 
 module.exports = function(app) {
 
+    // route to the default home page
     app.get('/', function (req, res) {
-        var locations = [
-            {
-                href: '/jobs/',
-                title: 'JOBS'
-            },
-            {
-                href: '/teaching-resources/',
-                title: 'RESOURCES'
-            },
-            {
-                href: 'http://community.tes.co.uk',
-                title: 'COMMUNITY'
-            },
-            {
-                href: 'http://news.tes.co.uk',
-                title: 'SCHOOL NEWS'
-            },
-            {
-                href: 'http://news.tes.co.uk/further-education/',
-                title: 'FE NEWS'
-            }
+        var menuItems =
+        [
+            {   href: '/jobs/', title: 'JOBS'},
+            {   href: '/teaching-resources/', title: 'RESOURCES'},
+            {   href: 'http://community.tes.co.uk', title: 'COMMUNITY'},
+            {   href: 'http://news.tes.co.uk', title: 'SCHOOL NEWS' },
+            {   href: 'http://news.tes.co.uk/further-education/', title: 'FE NEWS'}
         ];
 
-        res.render('home',{locationsData: locations});
+        res.render('home',{menuData: menuItems});
     });
 
+    // route to /about page
     app.get('/about', function (req, res) {
 
-        var fortunes = [
-            "Conquer your fears or they will conquer you.", "Rivers need springs.",
+        var fortunes =
+        [
+            "Conquer your fears or they will conquer you.",
+            "Rivers need springs.",
             "Do not fear what you don't know.",
-            "You will have a pleasant surprise.", "Whenever possible, keep it simple.",
+            "You will have a pleasant surprise.",
+            "Whenever possible, keep it simple.",
         ];
+
         var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
         res.render('about', {fortune: randomFortune});
 
