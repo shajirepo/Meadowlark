4f0fc7bdb04af28a6f1d45057880afe0775c5d97
shaji@shaji.co.uk <5hajiGit>
2014-11-18 20:22:17 +0000

    add credential

---
+++ model/credentials.js
@@ -0,0 +1,13 @@
+/**
+ * Created by admin on 17/11/2014.
+ */
+module.exports = {
+    mongo: {
+        development: {
+            connectionString: 'mongodb://tes:tesdb@ds063889.mongolab.com:63889/shaji-test-db'
+        },
+        production: {
+            connectionString: 'mongodb://<dbuser>:<dbpassword>@ds063889.mongolab.com:63889/shaji-test-db'
+        }
+    }
+};
